f2e8d785618713dfd62419806ac214244f639350
Dave Supolík <motivationwithdave@gmail.com>
2025-11-24 20:42:03 +0100

    feat: Implementace nové komponenty - Contact

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,15 @@ import gsap from "gsap";
 import { Draggable } from "gsap/Draggable";
 
 import { Dock, Navbar, Welcome } from "@components";
-import { Finder, Image, Resume, Safari, Terminal, Text } from "@windows";
+import {
+  Finder,
+  Image,
+  Resume,
+  Safari,
+  Terminal,
+  Text,
+  Contact,
+} from "@windows";
 
 gsap.registerPlugin(Draggable);
 
@@ -18,6 +26,7 @@ const App = () => {
       <Finder />
       <Text />
       <Image />
+      <Contact />
     </main>
   );
 };
